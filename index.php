6c8dbeae5a3615a963264cd5a73ce2686c4b4ced
subwafers <arguellesenzo1028@gmail.com>
2023-11-09 03:05:11 +0800

    3am

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include('include/header.php');?>
-<div class="text-center">
+<div class="text-center bg-warning">
     <h2 class= "m-4"></h2>
     <img src="image/purps.jpg" style="width:900px; height:500px;" class="img-fluid">
 </div>
