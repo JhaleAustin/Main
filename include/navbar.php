73aac9a7d24ade2d80b21e8b63ce393a240734fc
subwafers <arguellesenzo1028@gmail.com>
2023-11-09 02:01:39 +0800

    patch notes: Added stuff at the nav bar

--- include/navbar.php
+++ include/navbar.php
@@ -14,16 +14,18 @@
           <a class="nav-link" href="#">Apply for entrance EXAM here</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link ">Link</a>
+          <a class="nav-link " href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">FAQ</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
+            Admissions
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Enrollment Requirements</a></li>
+            <li><a class="dropdown-item" href="#">Admission FAQs</a></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include('include/header.php');?>
 <div class="text-center">
-    <h1 class = "mt-4">Wellcome Student!</h1>
+    <h1 class = "mt-4">Welcome Student!</h1>
     <h2 class= "m-4">register here to take a Entrance Exam NOW!</h2>
 </div>
 <?php include('include/footer.php'); ?>
