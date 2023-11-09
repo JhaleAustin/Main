acb0421b5d0a125647fe764845f74829d760cd37
subwafers <arguellesenzo1028@gmail.com>
2023-11-10 02:17:21 +0800

    new design

--- include/navbar.php
+++ include/navbar.php
@@ -1,24 +1,24 @@
-<nav class="navbar navbar-expand-lg bg-primary border border-black">
+<nav class="navbar navbar-expand-lg border border-black " style="background-color: #0504aa ">
   <div class="container-fluid">
-  <img src="image/logo.png" style="width:117px; height:90px;" class="img-fluid" alt="...">
-    <a class="navbar-brand" href="#">Student Personnel Services UPHSL</a>
+  <img src="image/SPS_Logo.png" style="width:135px; height:90px;" class="img-fluid" alt="...">
+    <a class="navbar-brand text-white" href="#">Student Personnel Services UPHSL</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll" >
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Apply for entrance EXAM here</a>
+          <a class="nav-link text-white" href="#">Apply for entrance EXAM here</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">About</a>
+        <li class="nav-item ">
+          <a class="nav-link text-white" href="#">About</a>
         </li>
       
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Admissions
           </a>
           <ul class="dropdown-menu">
--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php include('include/header.php');?>
-<div class="text-center bg-warning">
-    <h2 class= "m-4"></h2>
-    <img src="image/purps.jpg" style="width:900px; height:500px;" class="img-fluid">
+<style>
+  .carousel-inner .carousel-item img {
+    height: 450px; 
+    object-fit: cover;
+  }
+</style>
+<div class="container text-center bg-warning" style="margin-top: 60px; padding-left: 175px; padding-right: 175px;">
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  </div>
+  
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/purps.jpg" alt="Los Angeles" class="d-block w-100 ">
+    </div>
+    <div class="carousel-item">
+      <img src="image/test1.jpg" alt="Chicago" class="d-block w-100 ">
+    </div>
+    <div class="carousel-item">
+      <img src="image/pic1.jpg" alt="New York" class="d-block w-100 ">
+    </div>
+  </div>
+  
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
 </div>
+</div>
+
 <?php include('include/footer.php'); ?>
