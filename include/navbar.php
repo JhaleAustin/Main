4aea37cf2a1c8aef298f7c9c45d87fce5629a125
Percival <overken222@gmail.com>
2024-04-02 21:44:43 +0800

    adding proof of payemnt

--- include/navbar.php
+++ include/navbar.php
@@ -1,62 +1,71 @@
+<div class="navigationbar">
+  <div class="div" style="background-color:#FFEF00;font-family: 'Montserrat', sans-serif;">
+    <img src="image/SPS_Logo.png" style="width:175px ; height:115px; " class="img-fluid rounded mx-auto d-block" alt="...">
+    <p class="m-0 navbar-brand text-black text-center fs-3" href="index.php">Student Personnel Services UPHSL</p>
+  </div>
+  <nav class="navbar navbar-expand-lg  text-center " style="background-color:#002366;font-family: 'Montserrat', sans-serif;">
+    <div class="container-fluid justify-content-center">
 
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse flex-grow-0 " id="navbarScroll">
+        <ul class="navbar-nav my-2 my-lg-0 navbar-nav-scroll text-center fs-5" style="--bs-scroll-height: 100px;">
+          <div>
+            <li class="nav-item">
+              <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
+            </li>
+          </div>
 
-<div class= "navigationbar">
-<div class="div" style="background-color:#FFEF00;font-family: 'Montserrat', sans-serif;">
-  <img src="image/SPS_Logo.png" style="width:175px ; height:115px; " class="img-fluid rounded mx-auto d-block" alt="...">
-  <p class="m-0 navbar-brand text-black text-center fs-3" href="index.php">Student Personnel Services UPHSL</p>
-</div>  
-<nav class="navbar navbar-expand-lg  text-center " style="background-color:#002366;font-family: 'Montserrat', sans-serif;">
-  <div class="container-fluid justify-content-center">
+          <div class="apply">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="applyforexam.php">Apply for entrance EXAM </a>
+            </li>
+          </div>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse flex-grow-0 " id="navbarScroll">
-      <ul class="navbar-nav my-2 my-lg-0 navbar-nav-scroll text-center fs-5" style="--bs-scroll-height: 100px;">
-        <li class="nav-item">
-          <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="applyforexam.php">Apply for entrance EXAM </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link text-white" href="about.php">About</a>
-        </li>
-        <?php
-        if(isset($_SESSION['auth']))
-        {
-          ?>
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           <?= $login_session;?>
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="profile.php">profile</a></li>
-            <li><a class="dropdown-item" href="#"> my Reference number</a></li>
-            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-          </ul>
-        </li>
+          <div class="proofpayment">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="proofpayment.php">Upload Proof of payment</a>
+            </li>
+          </div>
+
+          <div class="about">
+            <li class="nav-item ">
+              <a class="nav-link text-white" href="about.php">About</a>
+            </li>
+          </div>
 
           <?php
-        }
-        else
-        {
+          if (isset($_SESSION['auth'])) {
           ?>
-         <a href="login.php" class="btn  btn-primary text-black text-decoration-none btn-block float-end fs-5">Log-in</a>;
-         <?php
-        }
-        ?>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <?= $login_session; ?>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="profile.php">profile</a></li>
+                <li><a class="dropdown-item" href="#"> my Reference number</a></li>
+                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+              </ul>
+            </li>
 
-        
-        
-      </ul>
+          <?php
+          } else {
+          ?>
+            <a href="login.php" class="btn  btn-primary text-black text-decoration-none btn-block float-end fs-5">Log-in</a>;
+          <?php
+          }
+          ?>
 
-      
+
+
+        </ul>
 
 
 
+
+
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </div>
-
